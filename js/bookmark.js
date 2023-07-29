27d0adfd2b2a3f868bcb4d206633a5962961a03c
OOO <66418642+aayeong@users.noreply.github.com>
2023-07-30 00:19:43 +0900

    Update bookmark & result page

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,20 +1,36 @@
-const starIcons = document.querySelectorAll('.bookmark__star-icon i');
+/* 즐겨찾기 경로 클릭 시 페이지 전환 but 별 아이콘 클릭 시 즐겨찾기 해제 */
+const bookmarkElements = document.querySelectorAll(".bookmark-component");
 
-function handleStarIcon(event) {
-  const FULL_STAR = "fa-solid";
-  const EMPTY_STAR = "fa-regular";
+function handleClickEvent(event) {
+    const FULL_STAR = "fa-solid";
+    const EMPTY_STAR = "fa-regular";
+    const isStarClicked = event.target.classList.contains("fa-star");
 
-  const iconElement = event.currentTarget;
+    if (isStarClicked) {
+      const icon = event.target;
+      const isRegular = icon.classList.contains(EMPTY_STAR);
 
-  if (iconElement.classList.contains(FULL_STAR)) {
-    iconElement.classList.remove(FULL_STAR);
-    iconElement.classList.add(EMPTY_STAR);
-  } else {
-    iconElement.classList.remove(EMPTY_STAR);
-    iconElement.classList.add(FULL_STAR);
-  }
+      if (isRegular) {
+        icon.classList.remove(EMPTY_STAR);
+        icon.classList.add(FULL_STAR);
+      } else {
+        icon.classList.remove(FULL_STAR);
+        icon.classList.add(EMPTY_STAR);
+
+        /* 일시적으로 없어짐, 새로고침 시 복구됨(추후에 서버랑 연동해야 함) */
+        const parentDiv = icon.closest(".bookmark-component");
+        if (parentDiv) {
+          parentDiv.classList.add('fade-out-done');
+          setTimeout(() => {
+            parentDiv.remove();
+          }, 600);
+        }
+      }
+    } else {
+      window.location.href = "pposongtime.html";
+    }
 }
 
-starIcons.forEach(icon => {
-  icon.addEventListener('click', handleStarIcon);
+bookmarkElements.forEach(bookmarkElement => {
+  bookmarkElement.addEventListener('click', handleClickEvent);
 });
